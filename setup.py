fa45ff7f0a41f3e783495165543cbf33cded26d0
Nicolas Triquenaux <nicolas.triquenaux@dataiku.com>
2020-10-26 08:50:45 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+import os
+from setuptools import setup
+
+
+setup(name='test-pip',
+      version='0.1',
+      description='(private) somedesc',
+      url='',
+      author='dataiku',
+      author_email='qa@dataiku.com',
+      license='',
+      packages=["test-pip"],
+      zip_safe=False)
